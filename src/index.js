b6fcfc08b596a893491fba4bf07021a2df06c35c
Ben <beanno1116@gmail.com>
2022-10-07 16:00:12 -0400

    added total and save items function

--- src/index.js
+++ src/index.js
@@ -20,6 +20,12 @@ const state = {
     },
 }
 
+const getTotal = () => {
+    return filteredData.reduce((acc,curr) => {
+        return acc + +curr.price;
+    },0);
+}
+
 const getCheapestItem = () => {
     
     return filteredData.reduce((acc,curr) => {
@@ -70,6 +76,21 @@ const displayMostExpensiveItem = () => {
     parent.appendChild(div);
 }
 
+const addSvg = () => {
+    state.items.forEach(i => {
+        const svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+        const path = document.createElementNS("http://www.w3.org/2000/svg","path");
+        svg.setAttribute('viewbox','0 0 24 24');
+        svg.setAttribute('height','24px');
+        svg.setAttribute('width','24px');
+        path.setAttribute('d','M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z');
+        svg.appendChild(path);
+        const div = document.getElementById('trash-' + i.id);
+        div.appendChild(svg);
+
+    })
+}
+
 const buildDeleteLinks = () => {
     const deletes = document.querySelectorAll('td[data-delete');
     for(let del of deletes){
@@ -115,14 +136,15 @@ const buildTable = () => {
 
     filteredData.map(item => {
         const {name,id,price,category,size} = item;
-        html += `<tr><td>${name}</td><td>${size}</td><td>${formatMoney(price)}</td><td>${category}</td><td id="tr-${id}" style="cursor:pointer;" data-delete="${id}">Delete</td></tr>`
+        html += `<tr><td>${name}</td><td>${size}</td><td>${formatMoney(price)}</td><td>${category}</td><td id="tr-${id}" style="cursor:pointer;" data-delete="${id}"><div style="text-align:center;" id="trash-${id}"></div></td></tr>`
     })
-
+    html += `<tr><td colspan="2"></td><td>${formatMoney(getTotal())}</td><td colspan="2"></td></tr>`;
     html += '</table>';
     document.getElementById('items').innerHTML = html;
     buildDeleteLinks();
     displayCheapestItem();
     displayMostExpensiveItem();
+    addSvg();
 }
 
 buildTable();
@@ -151,11 +173,11 @@ const handleFilterChange = (e) => {
 const buildFilterBox = () => {
     const categories = data.unique('category');
 
-    let html = '<select id="category-filter"><option value="0">Select a category to filter</option>'
+    let html = '<select id="category-filter"><option value="0">Select a category to filter</option>';
     categories.map(c => {
         html += `<option value="${c}">${c}</option>`;
     })
-    html += '</select>';
+    html += "</select>";
     document.getElementById('filter').innerHTML = html;
     const newSelect = document.getElementById('category-filter');
     newSelect.addEventListener('change',handleFilterChange);
@@ -173,3 +195,34 @@ const deleteItem = id => {
         buildTable();
     }
 }
+
+const saveButton = document.getElementById('save-item');
+
+const clearForm = () => {
+    Object.keys(state.currentItem).map(key => {
+        document.getElementById(key).value = "";
+    })
+}
+
+const saveItem = () => {
+    const copyItems = [...state.items,state.currentItem];
+    state.items = copyItems;
+    filteredData = copyItems;
+    clearForm();
+    buildTable();
+}
+saveButton.addEventListener('click',saveItem);
+
+const createItemCategory = () => {
+    const categories = data.unique('category');
+    let html = `<select id="category"><option value="0">Select a category</option>`;
+    categories.map(c => {
+        html += `<option value="${c}">${c}</option>`;
+    })
+    html += "</select>";
+    document.getElementById('item-category').innerHTML = html;
+    const newSelect = document.getElementById('category');
+    newSelect.addEventListener('change',changeState);
+}
+
+createItemCategory();
